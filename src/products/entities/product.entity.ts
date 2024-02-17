67541f147a6b895d5346854c4c561383cb33a6f5
gacc94 <gustavocaqui94@gmail.com>
2024-02-17 11:15:31 -0500

    agregando entitdad product

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,34 @@
-export class Product {}
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({
+  schema: 'public',
+})
+export class Product {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', { unique: true })
+  title: string;
+
+  @Column('numeric', { default: 0 })
+  price: number;
+
+  @Column('text', { nullable: true })
+  description: string;
+
+  @Column('text', { unique: true })
+  slug: string;
+
+  // @Column('text', { unique: true })
+  // sku: string;
+
+  @Column('int', { default: 0 })
+  stock: number;
+
+  @Column('text', { array: true, nullable: true })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
+}
