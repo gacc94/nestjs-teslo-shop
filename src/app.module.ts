3fb18b91cb391c674a2f2cebb6e03f820cfe85c4
gacc94 <gustavocaqui94@gmail.com>
2024-02-17 10:51:41 -0500

    agregando modulo de productos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      cache: true,
+      ignoreEnvFile: false,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,//TODO: localhost,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+      keepConnectionAlive: true,
+      logging: true,
+      logger: 'file'
+    }),
+    ProductsModule
+  ],
 })
-export class AppModule {}
+export class AppModule { }
